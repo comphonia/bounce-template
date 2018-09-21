ab558351fabf45f8c4da84fca07bf1feca45fc0c
comphonia <online.comphonia@gmail.com>
2018-09-20 21:58:54 -0500

    Initial Commit - greyboxing

---
+++ bounce/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+   [SerializeField] private GameObject player;
+    [SerializeField]float force;
+    Rigidbody2D rbPlayer;
+
+    private void Start()
+    {
+        player = gameObject;
+        rbPlayer = player.GetComponent<Rigidbody2D>();
+    }
+
+    public void LeftForce()
+    {
+        print("left click");
+        rbPlayer.AddForce(Vector2.left * force);
+    }
+
+    public void RightForce()
+    {
+        print("right click");
+        rbPlayer.AddForce(Vector2.right * force);
+    }
+}
